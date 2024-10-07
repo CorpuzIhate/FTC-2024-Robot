653fdaf8edc72fd174c6ff508dc29c99733f5b53
CorpuzIhate <guyihate@gmail.com>
2024-10-07 14:14:52 -0400

    -Refactored Tele-op subsystems was successful -tuned PID constants for shoulder PID Controller

--- TeamCode/src/main/java/PidShoulderTest.java
+++ TeamCode/src/main/java/PidShoulderTest.java
@@ -4,22 +4,24 @@ import com.arcrobotics.ftclib.controller.PIDFController;
 import com.arcrobotics.ftclib.controller.wpilibcontroller.ArmFeedforward;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Config
-
+@TeleOp
 public class PidShoulderTest extends OpMode {
     public static double currentSetpoint;
 //TODO tune coefficients.
-    public static double kP = 1;
+    public static double kP = 0.6;
     public static double kI = 0;
-    public static double kD = 1;
-    public static double kF = 1;
+    public static double kD = 0.015;
+    public static double kF = 0;
     PIDFController feedforward;
-    ArmFeedforward test;
+
 
     double output;
+    boolean shoulderisAtpoint;
 
     private final FtcDashboard dashboard = FtcDashboard.getInstance();
     private final Telemetry dashboardTelemetry = dashboard.getTelemetry();
@@ -30,15 +32,32 @@ public class PidShoulderTest extends OpMode {
         feedforward = new PIDFController(kP, kI, kD, kF);
 
         shoulderMotor = new Motor(hardwareMap,"shoulder_motor");
+        shoulderMotor.resetEncoder();
         shoulderMotor.setRunMode(Motor.RunMode.RawPower);
     }
     public void loop(){
         output = feedforward.calculate(shoulderMotor.getCurrentPosition(), currentSetpoint);
 
+        dashboardTelemetry.addData("kP",kP);
+        dashboardTelemetry.addData("kI",kI);
+        dashboardTelemetry.addData("kD",kD);
+        dashboardTelemetry.addData("kF",kF);
+
+        dashboardTelemetry.addData("position",shoulderMotor.getCurrentPosition());
+
+
+        dashboardTelemetry.update();
         if(feedforward.atSetPoint()){
-            output = 0;
+            shoulderisAtpoint = true;
+            dashboardTelemetry.addData("atPoint","yes");
+            shoulderMotor.stopMotor();
+
+
         }
-        shoulderMotor.set(output);
+        if(!shoulderisAtpoint){
+            shoulderMotor.set(output);
+        }
+
 
     }
 
